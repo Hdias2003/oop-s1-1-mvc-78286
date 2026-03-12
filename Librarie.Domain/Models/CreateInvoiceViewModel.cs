3069d4f130240e61bb006b69215ae9d9911db5f0
Hdias2003 <hdiasnascimento@gmail.com>
2026-03-12 18:25:43 +0000

    create and details for all models added

---
+++ Librarie.Domain/Models/CreateInvoiceViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Librarie.Domain.Models
+{
+    public class CreateInvoiceViewModel
+    {
+        [Required]
+        [Display(Name = "Invoice Date")]
+        [DataType(DataType.Date)]
+        public DateTime InvoiceDate { get; set; } = DateTime.Today;
+
+        [Required]
+        [Display(Name = "Customer")]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [Display(Name = "Product")]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Range(1, 100000)]
+        public int Quantity { get; set; }
+
+        public List<SelectListItem> Customers { get; set; } = new();
+        public List<SelectListItem> Products { get; set; } = new();
+    }
+}
